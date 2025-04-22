f38c9055e29dd8193dc24b07408fec7a83a2ab86
fibodevy <fibodevy@gmail.com>
2025-04-23 00:08:48 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+## Lazarus - Toggle Display Affinity
+
+Adds a "Toggle Display Affinity" option in the **View** menu that allows to prevent taking a screenshot of the IDE window.
+
+Thanks [@n7800](https://forum.lazarus.freepascal.org/index.php/topic,70891.msg552920.html#msg552920) for putting this into an installation package.
---
+++ utoggledisplayaffinity.pas
@@ -0,0 +1,49 @@
+unit uToggleDisplayAffinity;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Math,
+  // LCL
+  LCLType,
+  // IdeIntf
+  LazIDEIntf, MenuIntf;
+
+procedure Register;
+
+implementation
+
+{$ifdef WINDOWS}
+const // todo: resourcestring
+  lisMenuViewToggleDisplayAffinity = 'Toggle Display Affinity';
+
+var
+  itmViewToggleDisplayAffinity: TIDEMenuCommand = nil;
+
+function GetWindowDisplayAffinity(hWnd: HWND; pdwAffinity: PDWORD): bool; stdcall;
+  external 'user32.dll';
+
+function SetWindowDisplayAffinity(hWnd: HWND; dwAffinity: DWORD): bool; stdcall;
+  external 'user32.dll';
+
+procedure ToggleDisplayAffinity(Sender: TObject);
+var
+  lAffinity: DWORD;
+begin
+  itmViewToggleDisplayAffinity.Checked := SetWindowDisplayAffinity(LazarusIDE.GetMainBar.Handle, IfThen(GetWindowDisplayAffinity(LazarusIDE.GetMainBar.Handle, @lAffinity) and (lAffinity = 0), $11, 0));
+end;
+{$endif}
+
+procedure Register;
+begin
+  {$ifdef WINDOWS}
+  itmViewToggleDisplayAffinity := RegisterIDEMenuCommand(itmViewMainWindows, 'ToggleDisplayAffinity',
+    lisMenuViewToggleDisplayAffinity, nil, @ToggleDisplayAffinity, nil);
+  itmViewMainWindows.Insert(0, itmViewToggleDisplayAffinity);
+  {$endif}
+end;
+
+end.
+
